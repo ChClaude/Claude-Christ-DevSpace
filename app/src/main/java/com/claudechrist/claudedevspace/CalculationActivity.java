d77b5d47603a99a3f0fd74acb9194622f85d8ca8
Claude Christ <christ.tchambila@gmail.com>
2019-03-31 21:39:17 +0200

    Initial commit

---
+++ app/src/main/java/com/claudechrist/claudedevspace/CalculationActivity.java
@@ -0,0 +1,18 @@
+package com.claudechrist.claudedevspace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculationActivity extends AppCompatActivity {
+
+    public void volumeOfSphere() {
+        //TODO: Code here the algorithm to compute the volume of sphere
+        // - sphere: (4/3) * PI * radius * radius * radius
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculation);
+    }
+}
