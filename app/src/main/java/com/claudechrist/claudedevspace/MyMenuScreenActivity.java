c83f0251e023908dbf372b8e2cb24a6399546a6c
Claude Christ <christ.tchambila@gmail.com>
2019-04-01 21:06:08 +0200

    Main2Acticity.java and its corresponding xml file renamed to MyMenuScreen

--- app/src/main/java/com/claudechrist/claudedevspace/MyMenuScreenActivity.java
+++ app/src/main/java/com/claudechrist/claudedevspace/MyMenuScreenActivity.java
@@ -3,11 +3,11 @@ package com.claudechrist.claudedevspace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class MyMenuScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_my_menu_sreen);
     }
 }
