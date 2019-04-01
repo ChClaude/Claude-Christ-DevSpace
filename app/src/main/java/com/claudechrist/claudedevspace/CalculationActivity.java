e026a8e4f1821137637d46c4f70da936368c7ed0
Claude Christ <christ.tchambila@gmail.com>
2019-04-01 23:24:27 +0200

    Claculation acitvity updated

--- app/src/main/java/com/claudechrist/claudedevspace/CalculationActivity.java
+++ app/src/main/java/com/claudechrist/claudedevspace/CalculationActivity.java
@@ -1,13 +1,37 @@
 package com.claudechrist.claudedevspace;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.InputMismatchException;
 
 public class CalculationActivity extends AppCompatActivity {
 
-    public void volumeOfSphere() {
+    public void volumeOfSphere(View view) {
         //TODO: Code here the algorithm to compute the volume of sphere
         // - sphere: (4/3) * PI * radius * radius * radius
+
+        EditText radiusEditText = findViewById(R.id.radiusEditText);
+        TextView calculationOutputTextView = findViewById(R.id.calculationOutputTextView);
+        int radius = 1;
+
+
+        try {
+                radius = Integer.parseInt(radiusEditText.getText().toString());
+        } catch (Exception e) {
+                Toast.makeText(this, "Enter an integer as radius", Toast.LENGTH_LONG).show();
+        }
+
+        // computing the volume of the sphere
+        double volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+        calculationOutputTextView.setText(String.format("%s %.2f", "The volume of the sphere is ", volume));
+        calculationOutputTextView.animate().alpha(1).setDuration(2000);
     }
 
     @Override
