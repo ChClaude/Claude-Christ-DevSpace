d79e2e5e303bc6d696be4b2935c5816e819a2adf
Claude Christ <christ.tchambila@gmail.com>
2019-04-01 22:40:24 +0200

    Code added in my MyMenuScreenActivity.java

--- app/src/main/java/com/claudechrist/claudedevspace/MyMenuScreenActivity.java
+++ app/src/main/java/com/claudechrist/claudedevspace/MyMenuScreenActivity.java
@@ -1,13 +1,33 @@
 package com.claudechrist.claudedevspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MyMenuScreenActivity extends AppCompatActivity {
 
+    public void toMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
+    public void toCalculationActivity(View view) {
+        Intent intent = new Intent(this, CalculationActivity.class);
+        startActivity(intent);
+    }
+
+    public void toMyDetailsActivity(View view) {
+        Intent intent = new Intent(this, MyDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    public void toMyProfileActivity(View view) {
+        Intent intent = new Intent(this, MyProfileActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
