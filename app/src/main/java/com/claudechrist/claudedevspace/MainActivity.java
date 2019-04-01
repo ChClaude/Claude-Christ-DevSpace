d72d9245cf413f03e2af7ef8617cb5e7b9d15469
Claude Christ <christ.tchambila@gmail.com>
2019-04-01 21:01:40 +0200

    General set up of the app

--- app/src/main/java/com/claudechrist/claudedevspace/MainActivity.java
+++ app/src/main/java/com/claudechrist/claudedevspace/MainActivity.java
@@ -1,9 +1,18 @@
 package com.claudechrist.claudedevspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void toProfileActivity(View view) {
+        Intent i = new Intent(this, MyProfileActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/claudechrist/claudedevspace/MyProfileActivity.java
+++ app/src/main/java/com/claudechrist/claudedevspace/MyProfileActivity.java
@@ -1,13 +1,23 @@
 package com.claudechrist.claudedevspace;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        ImageView mySelfieImageView = findViewById(R.id.mySelfieImageView);
+
+        Glide.with(this).load(R.drawable.my_selfie).apply(RequestOptions.circleCropTransform()).into(mySelfieImageView);
     }
 }
