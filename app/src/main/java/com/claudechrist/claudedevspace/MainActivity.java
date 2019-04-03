2f7060c19e72b925cf6c650059a86455dac133c2
Claude Christ <christ.tchambila@gmail.com>
2019-04-03 12:11:01 +0200

    Code added for editText filling in requirement

--- app/src/main/java/com/claudechrist/claudedevspace/MainActivity.java
+++ app/src/main/java/com/claudechrist/claudedevspace/MainActivity.java
@@ -2,26 +2,30 @@ package com.claudechrist.claudedevspace;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String  userName;
+    public static String userName;
 
     // Next activity toMyMenuScreen
     public void toMyMenuScreen(View view) {
-        Log.i("Info", "Next button clicked");
-        Intent i = new Intent(this, MyMenuScreenActivity.class);
-        startActivity(i);
         EditText visitorNameEditText = findViewById(R.id.visitorNameEditText);
 
-        userName = visitorNameEditText.getText().toString();
+        if (TextUtils.isEmpty(visitorNameEditText.getText())) {
+            visitorNameEditText.setError("Visitor name is required");
+        } else {
+            Intent i = new Intent(this, MyMenuScreenActivity.class);
+            startActivity(i);
+            visitorNameEditText = findViewById(R.id.visitorNameEditText);
+
+            userName = visitorNameEditText.getText().toString();
+        }
     }
 
     // Exiting the app
